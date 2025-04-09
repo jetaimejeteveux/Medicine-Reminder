dd63e7c7c09208c9a1c7f1334cc06cfd42cf70a4
jetaimejeteveux <akufirman22@gmail.com>
2025-04-09 19:48:08 +0700

    fix: cron not running

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { ScheduleModule } from '@nestjs/schedule';
 import { PrismaModule } from 'prisma/prisma.module';
 import { AuthModule } from 'src/modules/auth/auth.module';
 import { MedicineModule } from 'src/modules/medicines/medicine.module';
@@ -11,6 +12,7 @@ import { UsersModule } from 'src/modules/users/users.module';
     ConfigModule.forRoot({
       isGlobal: true,
     }),
+    ScheduleModule.forRoot(),
     PrismaModule,
     AuthModule,
     UsersModule,
