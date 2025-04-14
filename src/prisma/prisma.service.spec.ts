cdee1c8188f602fda77bb312ca2134ac78ed71af
jetaimejeteveux <akufirman22@gmail.com>
2025-04-14 21:41:48 +0700

    test: add test for prisma service

---
+++ src/prisma/prisma.service.spec.ts
@@ -0,0 +1,32 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { PrismaService } from './prisma.service';
+
+describe('PrismaService', () => {
+  let service: PrismaService;
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      providers: [PrismaService],
+    }).compile();
+
+    service = module.get<PrismaService>(PrismaService);
+
+    // Mocking methods that is called
+    jest.spyOn(service, '$connect').mockImplementation(async () => Promise.resolve());
+    jest.spyOn(service, '$disconnect').mockImplementation(async () => Promise.resolve());
+  });
+
+  it('should be defined', () => {
+    expect(service).toBeDefined();
+  });
+
+  it('should connect on module init', async () => {
+    await service.onModuleInit();
+    expect(service.$connect).toHaveBeenCalled();
+  });
+
+  it('should disconnect on module destroy', async () => {
+    await service.onModuleDestroy();
+    expect(service.$disconnect).toHaveBeenCalled();
+  });
+});
